136de40a80acbbdadec338b5d506994b11f300de
shaojiang <caishaojiang@gmail.com>
2024-04-17 18:41:37 +0800

    comment out unused font

--- src/theme/font.ts
+++ src/theme/font.ts
@@ -1,4 +1,5 @@
-import { Lato, Outfit, Space_Mono } from 'next/font/google';
+// import { Lato, Outfit, Space_Mono } from 'next/font/google';
+import { Outfit } from 'next/font/google';
 
 const outfitFont = Outfit({
   weight: ['300', '400', '500', '600', '700', '800', '900'],
@@ -6,17 +7,17 @@ const outfitFont = Outfit({
   variable: '--font-outfit',
 });
 
-const latoFont = Lato({
-  weight: ['100', '300', '400', '700', '900'],
-  subsets: ['latin'],
-  variable: '--font-lato',
-});
-
-const spaceMono = Space_Mono({
-  weight: ['400', '700'],
-  subsets: ['latin'],
-  variable: '--font-space-mono',
-});
+// const latoFont = Lato({
+//   weight: ['100', '300', '400', '700', '900'],
+//   subsets: ['latin'],
+//   variable: '--font-lato',
+// });
+//
+// const spaceMono = Space_Mono({
+//   weight: ['400', '700'],
+//   subsets: ['latin'],
+//   variable: '--font-space-mono',
+// });
 
 const fonts = {
   default: outfitFont,
