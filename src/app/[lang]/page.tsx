f9cc3ed1c45e8f8a3e03d51a08f7d6c6b0c92690
shaojiang <caishaojiang@gmail.com>
2024-04-22 10:32:11 +0800

    0 - bare blank next.js app

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,24 +1,24 @@
 import Image from "next/image";
 import Button from '@mui/material/Button';
 import { ParamsWithLng } from '@jsc/types/i18n';
-import { getTranslations } from '@jsc/i18n/translations';
-
-import { faUser } from '@fortawesome/pro-solid-svg-icons';
-import { faGear } from '@fortawesome/pro-duotone-svg-icons';
-import AwesomeIcon from '@jsc/components/AwesomeIcon';
+// import { getTranslations } from '@jsc/i18n/translations';
+//
+// import { faUser } from '@fortawesome/pro-solid-svg-icons';
+// import { faGear } from '@fortawesome/pro-duotone-svg-icons';
+// import AwesomeIcon from '@jsc/components/AwesomeIcon';
 
 export default async function Home({ params: { lang } }: { params: ParamsWithLng }) {
-  const messages = await getTranslations(lang);
+  // const messages = await getTranslations(lang);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
           <code className="font-mono font-bold">src/app/page.tsx</code>
-          <AwesomeIcon icon={faUser} size='lg' />
-          <AwesomeIcon icon={faGear} size='lg' />
+          {/*<AwesomeIcon icon={faUser} size='lg' />*/}
+          {/*<AwesomeIcon icon={faGear} size='lg' />*/}
           <span>
-            {messages?.common?.more}
+            {/*{messages?.common?.more}*/}
           </span>
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
